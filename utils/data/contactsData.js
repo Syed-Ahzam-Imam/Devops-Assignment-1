317fb3a04d109bd2d9badc97718db62e64c56239
Syed-Ahzam-Imam <ahzamimam92@gmail.com>
2024-05-18 21:53:53 +0500

    Initial commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'ahzamimam7388@gmail.com',
+    phone: '+8801608797655',
+    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
+    github: 'https://github.com/said7388',
+    facebook: 'https://www.facebook.com/ahzamimam.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/ahzam-imam-bd/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/ahzam-imam',
+    devUsername: "said7388"
+}
